c8087ea7919a6897934719fd0e859fc6f6a84cb0
Peter Vlk <petervlk86@gmail.com>
2021-11-24 09:35:33 +0100

    modify package system setup and automatic updates

--- init.el
+++ init.el
@@ -3,31 +3,43 @@
 ;;; Commentary:
 ;; Plain and simple init file
 
-(require 'package)
-
 ;;; Code:
 
-;; Set up emacs package archives with 'package
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
-(setq package-enable-at-startup nil)
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;; Ensure use-package is present. From here on out, all packages are loaded
-;; with use-package, a macro for importing and installing packages. Also, refresh the package archive on load so we can pull the latest packages.
+;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
 
 (require 'use-package)
+(setq use-package-always-ensure t ;; Makes sure to download new packages if they aren't already downloaded
+      use-package-verbose t) ;; Package install logging. Packages break, it's nice to know why.
+
+
+;; Automatic package updates
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
 
 ;; ESC Cancels All
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(setq use-package-always-ensure t ;; Makes sure to download new packages if they aren't already downloaded
-      use-package-verbose t) ;; Package install logging. Packages break, it's nice to know why.
 
 (use-package which-key
   :defer 0
