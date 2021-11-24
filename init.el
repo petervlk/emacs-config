2a23c0e6938d1caccefc481c08b46ca30f10b8b0
Peter Vlk <petervlk86@gmail.com>
2021-11-24 10:13:23 +0100

    add evil

--- init.el
+++ init.el
@@ -38,7 +38,7 @@
 
 
 ;;; UI
-;; get rid of clutter
+;; Basic UI
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (menu-bar-mode -1)          ; Disable the menu bar
@@ -50,6 +50,9 @@
 
 ;; show column number
 (setq column-number-mode t)
+
+;; visible bell insead of beeping
+(setq visible-bell t)
 
 ;; Themes
 (use-package all-the-icons
@@ -68,10 +71,45 @@
   :init (doom-modeline-mode 1))
 
 
-(setq visible-bell t)
-
+;;; Keybinding conf
 ;; ESC Cancels All
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(use-package general
+  :after evil
+  :config
+  (general-create-definer vlko/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC")
+
+  (vlko/leader-keys
+   "t"  '(:ignore t :which-key "toggles")
+   "tt" '(counsel-load-theme :which-key "choose theme")
+   "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/Emacs.org")))))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 
 (use-package which-key
