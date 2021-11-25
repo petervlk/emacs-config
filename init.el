775c45defecc3d551d5149d370992e3d35678766
Peter Vlk <petervlk86@gmail.com>
2021-11-25 23:32:08 +0100

    fix company binding

--- init.el
+++ init.el
@@ -174,13 +174,12 @@
   :after lsp-mode
   :hook (lsp-mode . company-mode)
   :bind (:map company-active-map
-              ("<tab>" . company-complete-selection))
-  (:map lsp-mode-map
-        ("<tab>" . company-indent-or-complete-common))
+         ("TAB" . company-complete-selection)
+         :map lsp-mode-map
+         ("TAB" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
-
 
 (use-package company-box
   :hook (company-mode . company-box-mode))
