e99a4787389368488a0819442e1dd8c54980dd75
Peter Vlk <petervlk86@gmail.com>
2021-11-26 17:26:42 +0100

    ediff uses buffer for displaying controlls instead of new frame

--- init.el
+++ init.el
@@ -206,7 +206,10 @@
 (use-package magit
   :commands magit-status
   :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
+  :config
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain)) ;;use control buffer instead of control frame for ediff
+
 
 (use-package magit-delta
   :hook (magit-mode . magit-delta-mode))
