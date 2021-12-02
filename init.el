590f47dd2395d7c6501bc1bf070c278e17d3c26c
Peter Vlk <petervlk86@gmail.com>
2021-12-02 16:39:45 +0100

    show line and column numbers

--- init.el
+++ init.el
@@ -77,8 +77,18 @@
 (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; show column number
-(setq column-number-mode t)
+;; show line and column number
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook
+		cider-repl-mode-hook
+		))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; visible bell insead of beeping
 (setq visible-bell t)
