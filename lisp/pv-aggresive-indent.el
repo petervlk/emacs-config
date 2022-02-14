be15fc2d76cc89bbdbb08ebbab9027fe5c1f80d7
Peter Vlk <petervlk86@gmail.com>
2022-02-14 12:05:02 +0100

    exclude aggresive-indent from clojure and cider modes

--- lisp/pv-aggresive-indent.el
+++ lisp/pv-aggresive-indent.el
@@ -8,7 +8,9 @@
 (use-package aggressive-indent
   :config
   (global-aggressive-indent-mode 1)
-  (add-to-list 'aggressive-indent-excluded-modes 'cider-repl-mode))
+  (add-to-list 'aggressive-indent-excluded-modes 'cider-mode)
+  (add-to-list 'aggressive-indent-excluded-modes 'cider-repl-mode)
+  (add-to-list 'aggressive-indent-excluded-modes 'clojure-mode))
 
 (provide 'pv-aggresive-indent)
 
