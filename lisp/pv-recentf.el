ebe466345c172ee97f723ee7f7ae71341c5596cf
Peter Vlk <petervlk86@gmail.com>
2022-02-27 02:45:23 +0100

    add recenf config

---
+++ lisp/pv-recentf.el
@@ -0,0 +1,13 @@
+;;; pv-recentf.el --- Summary
+
+;;; Commentary:
+;;; Setup config for showinf recent files
+;;; Code:
+
+(use-package recentf
+  :config
+  (recentf-mode 1))
+
+(provide 'pv-recentf)
+
+;;; pv-recentf.el ends here
