69243f7437c6ee2e28d433efaded5fa670b3f4fc
Peter Vlk <petervlk86@gmail.com>
2022-06-15 12:04:55 +0200

    add savehist package

---
+++ lisp/pv-savehist.el
@@ -0,0 +1,13 @@
+;;; pv-savehist.el --- Summary
+
+;;; Commentary:
+;;; Setup savehist
+;;; Code:
+
+(use-package savehist
+  :init
+  (savehist-mode))
+
+(provide 'pv-savehist)
+
+;;; pv-savehist.el ends here
